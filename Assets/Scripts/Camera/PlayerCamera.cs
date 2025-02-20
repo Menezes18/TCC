9e440b618c482d890f24fd3c66c55b5d684333b2
Menezes18 <gustavo.e.menezes@outlook.com>
2025-02-20 16:05:45 -0300

    Movimentação em 3 pessoa

--- Assets/Scripts/Camera/PlayerCamera.cs
+++ Assets/Scripts/Camera/PlayerCamera.cs
@@ -1,17 +1,19 @@
 using System;
 using Mirror;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerCamera : NetworkBehaviour
 {
     [Header("Configuração da Câmera")]
     [SerializeField] private PlayerControlSO PlayerControlSO;
     [SerializeField] private Vector3 cameraOffset = new Vector3(0, 0.5f, 0); 
-    
     private Camera _cam;
     private float _mouseX, _mouseY;
-    
+    public Transform CameraTarget;
+    public float TargetHeight = 1f;
+    public PlayerScript PlayerScript;
+    private Vector2 targetLook;
+    public Vector2 Clamp = new Vector2(-75, 75);
     private void Start()
     {
         if(base.isOwned == false) return;
@@ -36,18 +38,18 @@ public class PlayerCamera : NetworkBehaviour
         Vector2 mouseDelta = input * 0.1f;
         
         _mouseX += mouseDelta.x;
-        _mouseY = Mathf.Clamp(_mouseY - mouseDelta.y, -75f, 75f);
+        _mouseY = Mathf.Clamp(_mouseY - mouseDelta.y, Clamp.x, Clamp.y);
     }
 
     private void UpdateCameraPosition()
     {
-        Vector3 targetPosition = transform.position + cameraOffset;
-        _cam.transform.position = targetPosition;
+        CameraTarget.transform.position = PlayerScript.transform.position + Vector3.up * TargetHeight;
     }
 
     private void UpdateCameraRotation()
     {
-        transform.rotation = Quaternion.Euler(0, _mouseX, 0);
-        _cam.transform.rotation = Quaternion.Euler(_mouseY, _mouseX, 0);
+        CameraTarget.transform.rotation = Quaternion.Euler(_mouseY, _mouseX, 0);
+         transform.rotation = Quaternion.Euler(0, _mouseX, 0);
+        // _cam.transform.rotation = Quaternion.Euler(_mouseY, _mouseX, 0);
     }
 }
