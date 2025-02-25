90398cd4d83fbc04bc3ae7508890093665deab78
Arcmigs <miguel-felipe2010@hotmail.com>
2025-02-25 00:47:12 -0300

    feature - cria ChaoMae e ChaoCaindo

---
+++ Assets/Scripts/Minigames/2-Chão/ChaoCaindo.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using UnityEngine;
+
+class ChaoCaindo : ChaoMae
+{
+    public Vector3 posIncial;
+
+    void Awake()
+    {
+        posIncial = transform.position;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") && !chaoTirado){
+            chaoTirado = true;
+            tiraChao();
+        }
+    }
+    public override void poeChao()
+    {
+        transform.position = posIncial;
+    }
+
+    public override void tiraChao()
+    {
+        StartCoroutine(desceChao());
+    }
+
+    private IEnumerator desceChao()
+    {
+        float tempoDecorrido = 0f;
+        yield return new WaitForSeconds(0.6f);
+        while (tempoDecorrido < tempo)
+        {
+            transform.position -= Vector3.up * speed * Time.deltaTime;
+            tempoDecorrido += Time.deltaTime;
+            yield return null;
+        }
+        yield return new WaitForSeconds(5f);
+        poeChao();
+    }
+}
---
+++ Assets/Scripts/Minigames/2-Chão/ChaoMae.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ChaoMae : MonoBehaviour
+{
+    [SerializeField]
+    float _speed = 5;
+    public float speed 
+    { 
+        get { return _speed; } 
+        protected set { _speed = value; } 
+    }
+
+    [SerializeField]
+    float _tempo = 5;
+    public float tempo 
+    { 
+        get { return _tempo; } 
+        protected set { _tempo = value; } 
+    }
+    
+    [SerializeField]
+    bool _chaoTirado = false;
+    public bool chaoTirado 
+    { 
+        get { return _chaoTirado; } 
+        protected set { _chaoTirado = value; } 
+    }
+
+    public abstract void tiraChao();
+
+    public abstract void poeChao();
+}
