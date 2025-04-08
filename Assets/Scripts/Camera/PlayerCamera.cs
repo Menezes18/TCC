a0750f2b065a8faf3a4e0615966c429c02f784a5
Arcmigs <miguel-felipe2010@hotmail.com>
2025-04-08 09:37:52 -0300

    Unity 6, atualiza packages, atualiza cinemachine

--- Assets/Scripts/Camera/PlayerCamera.cs
+++ Assets/Scripts/Camera/PlayerCamera.cs
@@ -1,5 +1,5 @@
 using System;
-using Cinemachine;
+using Unity.Cinemachine;
 using Mirror;
 using UnityEngine;
 using UnityEngine.InputSystem;
--- Assets/Scripts/Camera/PlayerSetup.cs
+++ Assets/Scripts/Camera/PlayerSetup.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using Mirror;
-using Cinemachine;
+using Unity.Cinemachine;
 
 public class PlayerSetup : NetworkBehaviour
 {
