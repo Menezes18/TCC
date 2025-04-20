34ce8353fe9c155acbe6dd9ec11daf1abc027f91
Menezes18 <gustavo.e.menezes@outlook.com>
2025-04-19 21:17:09 -0300

    feat: chamando animacao quando pega no celular

--- Assets/Scripts/Player/PlayerManagerUI.cs
+++ Assets/Scripts/Player/PlayerManagerUI.cs
@@ -7,14 +7,18 @@ public class PlayerManagerUI : NetworkBehaviour
 {
 
     public PlayerControlSO playerControlSO;
-
+    private PlayerScript _playerScript;
+    private bool _valueCelular = false;
     private void Start()
     {
+        _playerScript = GetComponent<PlayerScript>();
         playerControlSO.EventOnCelularMenu += EventOnCelularMenu;
     }
 
     private void EventOnCelularMenu(InputAction.CallbackContext obj)
     {
+        _valueCelular = !_valueCelular;
         MainMenu.instance.ToggleCelular();
+        _playerScript._animator.SetBool("ActiveCelular",_valueCelular);
     }
 }
