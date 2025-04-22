a109117c8cdf599ff3c77552991b3acc5487bc2c
Menezes18 <gustavo.e.menezes@outlook.com>
2025-04-22 15:06:10 -0300

    fix: sincroniza toggle do celular - animação ActiveCelular em todos os clientes via Cmd/ClientRpc

--- Assets/Scripts/Player/PlayerManagerUI.cs
+++ Assets/Scripts/Player/PlayerManagerUI.cs
@@ -5,25 +5,38 @@ using UnityEngine.InputSystem;
 
 public class PlayerManagerUI : NetworkBehaviour
 {
-
     public PlayerControlSO playerControlSO;
-    private PlayerScript _playerScript;
-    private bool _valueCelular = false;
-    
     public GameObject celular;
+
+    private bool _valueCelular = false;
+    private PlayerScript _playerScript;
+
     private void Start()
     {
+        if (!isLocalPlayer) return;
+
         celular.SetActive(false);
         _playerScript = GetComponent<PlayerScript>();
         playerControlSO.EventOnCelularMenu += EventOnCelularMenu;
     }
 
-    private void EventOnCelularMenu(InputAction.CallbackContext obj)
+    private void EventOnCelularMenu(InputAction.CallbackContext ctx)
     {
-        Debug.LogError(this.gameObject.name);
         _valueCelular = !_valueCelular;
-        MainMenu.instance.ToggleCelular();
-        celular.SetActive(_valueCelular);
-        _playerScript._animator.SetBool("ActiveCelular",_valueCelular);
+        CmdSetCelular(_valueCelular);
+    }
+
+    [Command]
+    private void CmdSetCelular(bool activate)
+    {
+        _valueCelular = activate;
+        RpcSetCelular(activate);
+    }
+
+    [ClientRpc]
+    private void RpcSetCelular(bool activate)
+    {
+        celular.SetActive(activate);
+        _playerScript._animator.SetBool("ActiveCelular", activate);
     }
 }
